7be0dcb7545e289f54cece34edb9c4a6468f93d1
AnastasiiaPirus <a.pirus2000@gmail.com>
2024-12-15 21:10:49 -0500

    Add localhost link

--- appointments-frontend/src/services/api.ts
+++ appointments-frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const VITE_API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const VITE_API_BASE_URL = 'http://localhost:3000' // import.meta.env.VITE_API_BASE_URL;
 
 export const fetchAppointments = async (): Promise<any[]> => {
     const response = await axios.get(`${VITE_API_BASE_URL}/appointments`);
